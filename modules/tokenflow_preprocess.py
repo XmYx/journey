81cc587c65fdb53954e9e21bbe24e211f20c4f3d
Miklos Nagy <miklos.mnagy@gmail.com>
2023-09-10 15:14:59 +0200

    config saving

--- modules/tokenflow_preprocess.py
+++ modules/tokenflow_preprocess.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import multiprocessing
 import os
 from pathlib import Path
@@ -14,6 +15,8 @@ def plugin_tab(*args, **kwargs):
     # Initialize session state variables
     if "data_path" not in st.session_state:
         st.session_state.data_path = ""
+    if "save_path" not in st.session_state:
+        st.session_state.save_path = ""
     if "H" not in st.session_state:
         st.session_state.H = 512
     if "W" not in st.session_state:
@@ -32,8 +35,9 @@ def plugin_tab(*args, **kwargs):
         st.session_state.inversion_prompt = "a woman running"
 
     # UI components
-    st.title("Stable Diffusion Preprocessor")
+    # st.title("Stable Diffusion Preprocessor")
     st.session_state.data_path = st.text_input("Data Path:", st.session_state.data_path)
+    st.session_state.save_path = st.text_input("Save Path:", st.session_state.save_path)
     st.session_state.H = st.number_input("Height:", value=st.session_state.H)
     st.session_state.W = st.number_input("Width:", value=st.session_state.W)
     st.session_state.sd_version = st.selectbox("SD Version:", ["1.5", "2.0", "2.1", "ControlNet", "depth"])
@@ -43,13 +47,45 @@ def plugin_tab(*args, **kwargs):
     st.session_state.n_frames = st.number_input("Number of Frames:", value=st.session_state.n_frames)
     st.session_state.inversion_prompt = st.text_input("Inversion Prompt:", st.session_state.inversion_prompt)
 
+    # Configuration name entry
+    config_name = st.text_input("Configuration Name:", value="default_config")
+
+    # Save and Load Configuration buttons
+    configs_path = "configs/tokenflow"
+    os.makedirs(configs_path, exist_ok=True)
+    config_files = [f.stem for f in Path(configs_path).glob('*.json')]
+
+    selected_config = st.selectbox("Select a configuration to load:", config_files)
+
+    if st.button("Save Configuration"):
+        config_data = {
+            "data_path": st.session_state.data_path,
+            "H": st.session_state.H,
+            "W": st.session_state.W,
+            "save_path": st.session_state.save_path,
+            "sd_version": st.session_state.sd_version,
+            "steps": st.session_state.steps,
+            "batch_size": st.session_state.batch_size,
+            "save_steps": st.session_state.save_steps,
+            "n_frames": st.session_state.n_frames,
+            "inversion_prompt": st.session_state.inversion_prompt
+        }
+        with open(Path(configs_path) / f"{config_name}.json", 'w') as json_file:
+            json.dump(config_data, json_file)
+
+    if st.button("Load Configuration"):
+        with open(Path(configs_path) / f"{selected_config}.json", 'r') as json_file:
+            loaded_config = json.load(json_file)
+            for key, value in loaded_config.items():
+                setattr(st.session_state, key, value)
+
     if st.button("Run Preprocessing"):
         # Create an options object (this could be a dictionary or an argparse.Namespace)
         opt = argparse.Namespace(
             data_path=st.session_state.data_path,
             H=st.session_state.H,
             W=st.session_state.W,
-            save_dir='latents',
+            save_dir=st.session_state.save_path,
             sd_version=st.session_state.sd_version,
             steps=st.session_state.steps,
             batch_size=st.session_state.batch_size,
--- modules/tokenflow_sdedit.py
+++ modules/tokenflow_sdedit.py
@@ -1,6 +1,10 @@
-import streamlit as st
+import json
+import os
+from pathlib import Path
 
-from src.TokenFlow.run_tokenflow_sdedit import run
+import streamlit as st
+from src.TokenFlow.run_tokenflow_sdedit import run as run_sdedit
+from src.TokenFlow.run_tokenflow_pnp import run as run_pnpedit
 
 # Import other required modules
 
@@ -10,6 +14,8 @@ plugin_info = {"name": "Stable Diffusion Processor"}
 def plugin_tab(*args, **kwargs):
     if "output_path" not in st.session_state:
         st.session_state.output_path = "output/path"
+    if "save_path" not in st.session_state:
+        st.session_state.save_path = ""
     if "n_timesteps" not in st.session_state:
         st.session_state.n_timesteps = 50
     if "start" not in st.session_state:
@@ -34,6 +40,48 @@ def plugin_tab(*args, **kwargs):
     st.session_state.negative_prompt = st.text_input("Negative Prompt:", st.session_state.negative_prompt)
     st.session_state.guidance_scale = st.number_input("Guidance Scale:", min_value=0.0,
                                                       value=st.session_state.guidance_scale)
+    method = st.selectbox('Method', ['PNP', 'SDEdit'])
+    st.session_state.save_path = st.text_input("Preprocessed Latent Path:", st.session_state.save_path)
+
+    # Configuration name entry
+    config_name = st.text_input("Configuration Name:", value="default_config")
+
+    # Save and Load Configuration buttons
+    configs_path = "configs/tokenflow_edit"
+    os.makedirs(configs_path, exist_ok=True)
+    config_files = [f.stem for f in Path(configs_path).glob('*.json')]
+
+    selected_config = st.selectbox("Select a configuration to load:", config_files)
+
+    if st.button("Save Configuration"):
+        config_data = {
+            "output_path": st.session_state.output_path,
+            "data_path": st.session_state.data_path,
+            "n_timesteps": st.session_state.n_timesteps,
+            "start": st.session_state.start,
+            "use_ddim_noise": st.session_state.use_ddim_noise,
+            "prompt": st.session_state.prompt,
+            "negative_prompt": st.session_state.negative_prompt,
+            "guidance_scale": st.session_state.guidance_scale,
+            "save_path": st.session_state.save_path,
+            # Add other parameters if needed
+            "H": st.session_state.H,
+            "W": st.session_state.W,
+            "sd_version": st.session_state.sd_version,
+            "steps": st.session_state.steps,
+            "batch_size": st.session_state.batch_size,
+            "save_steps": st.session_state.save_steps,
+            "n_frames": st.session_state.n_frames,
+            "inversion_prompt": st.session_state.inversion_prompt
+        }
+        with open(Path(configs_path) / f"{config_name}.json", 'w') as json_file:
+            json.dump(config_data, json_file)
+
+    if st.button("Load Configuration"):
+        with open(Path(configs_path) / f"{selected_config}.json", 'r') as json_file:
+            loaded_config = json.load(json_file)
+            for key, value in loaded_config.items():
+                setattr(st.session_state, key, value)
 
     if st.button("Run Processing"):
         # Here, gather all the previous state variables, and any other required information
@@ -57,7 +105,10 @@ def plugin_tab(*args, **kwargs):
             "inversion_prompt": st.session_state.inversion_prompt,
             "seed": 1,
             "device": "cuda",
-            "latents_path": "latents"
+            "latents_path": st.session_state.save_path
         }
         # Call your main function, e.g., run_processing
-        run(config)
+        if method == 'SDEdit':
+            run_sdedit(config)
+        else:
+            run_pnpedit(config)
